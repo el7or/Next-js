8b535198242410ec4d2c0ce6019d978bd8db8620
Ahmed Alhor <ahor.c@tetco.sa>
2022-05-08 20:40:10 +0200

    clean up

--- pages/index.js
+++ pages/index.js
@@ -19,19 +19,6 @@ function HomePage(props) {
   )
 }
 
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-
-//   // fetch data from an API
-
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS
-//     }
-//   };
-// }
-
 export async function getStaticProps() {
   // fetch data from an API
   const client = await MongoClient.connect(
